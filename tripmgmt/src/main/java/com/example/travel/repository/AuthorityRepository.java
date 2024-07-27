f047e86cbddfb6429b46d333fcf12a19cfe0bdae
ShreyaPandita <shreya.pandita@gmail.com>
2024-07-27 11:18:47 -0700

    Delete tripmgmt directory

--- tripmgmt/src/main/java/com/example/travel/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travel.repository;
-
-import com.example.travel.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
